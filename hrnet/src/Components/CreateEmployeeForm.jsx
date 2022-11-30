22e17e3b278668a7a2948723b96cd602362e0a05
BenoitMarechal <benoit.marechal.56@gmail.com>
2022-11-30 20:30:33 +0100

    dev ok

--- hrnet/src/Components/CreateEmployeeForm.jsx
+++ hrnet/src/Components/CreateEmployeeForm.jsx
@@ -3,7 +3,8 @@ import SingleTextInput from './SingleTextInput';
 
 const CreateEmployeeForm = () => {
   let basicData2 = ['First Name', 'Last Name', 'Date of Birth', 'Start Date'];
-  // console.log(basicData2);
+
+  console.log(basicData2);
   return (
     <form action='#' id='create-employee'>
       {basicData2.map((...elt) => (
