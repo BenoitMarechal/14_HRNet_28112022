1d27e7eed86033885b7c3368537c2576ef3e3dc4
BenoitMarechal <benoit.marechal.56@gmail.com>
2022-12-14 12:29:52 +0100

    reinstall npm

--- hrnet/src/Components/DataTable/EmployeeTable.jsx
+++ hrnet/src/Components/DataTable/EmployeeTable.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import DataTable from 'react-data-table-component';
 import { useSelector } from 'react-redux';
-import CustomMaterialPagination from '../CustomPagination/CustomPagination';
+//import CustomMaterialPagination from '../CustomPagination/CustomPagination';
 
 const EmployeeTable = () => {
   const allEmployees = useSelector((state) => state.dataBaseReducer.dataBase);
@@ -60,7 +60,7 @@ const EmployeeTable = () => {
       columns={columns}
       data={allEmployees}
       pagination
-      paginationComponent={CustomMaterialPagination}
+      //paginationComponent={CustomMaterialPagination}
     />
   );
 };
