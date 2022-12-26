a71ffbca36edae9515293b2480f2e570837669d4
BenoitMarechal <benoit.marechal.56@gmail.com>
2022-12-26 15:32:09 +0100

    validation strings ok

--- hrnet/src/service/formValidation.js
+++ hrnet/src/service/formValidation.js
@@ -1,4 +1,27 @@
+//--------REGEXP
+//Regexp for letter validation (first and last names)
+let letter = new RegExp(
+  /^[A-ZÀ-ÖØ]+(([',. -][a-zA-ZÀ-ÖØ-öø-ÿ])?[a-zA-ZÀ-ÖØ-öø-ÿ]*)*$/
+);
+//-------------VERIFICATION FUNCTIONS
+//Function that runs the letter Regexp:
+function stringCheck(stringToCheck) {
+  if (
+    stringToCheck.match(letter) !== null && //checks if the amount of characters matching the regexp is not null //AND
+    stringToCheck.length > 1
+  ) {
+    //input has at least three characters,
+
+    return true;
+  } else {
+    return false;
+  }
+}
+
 export function checkFormValidity() {
+  //---------limit dates
+  //World's oldest personn birthdate: 1903, Jan 2nd;
+
   let result = {};
   let firstNameInputValue = document.getElementById('first-name').value;
   let lastNameInputValue = document.getElementById('last-name').value;
@@ -13,13 +36,29 @@ export function checkFormValidity() {
   let departmentInputValue =
     document.getElementById('department').lastChild.firstChild.firstChild
       .textContent;
-  console.log(firstNameInputValue);
-  console.log(lastNameInputValue);
-  console.log(birthDateInputValue);
-  console.log(startDateInputValue);
-  console.log(streetInputValue);
-  console.log(cityInputValue);
-  console.log(stateInputValue);
-  console.log(zipCodeInputValue);
-  console.log(departmentInputValue);
+
+  //first name
+  if (stringCheck(firstNameInputValue) === true) {
+    result.firstNameError = '';
+  } else {
+    result.firstNameError =
+      'Field must contain at least two letter, start with a capital letter and have no special characters';
+  }
+  //last name
+  if (stringCheck(lastNameInputValue) === true) {
+    result.lastNameError = '';
+  } else {
+    result.lastNameError =
+      'Fields must contain at least two letter, start with a capital letter and have no special characters';
+  }
+
+  //city
+  if (stringCheck(cityInputValue) === true) {
+    result.cityError = '';
+  } else {
+    result.cityError =
+      'Field must contain at least two letter, start with a capital letter and have no special characters';
+  }
+
+  console.log(result);
 }
