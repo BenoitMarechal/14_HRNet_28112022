7902c34d1346d845018284d1680187b3685987a6
BenoitMarechal <benoit.marechal.56@gmail.com>
2023-01-25 14:49:49 +0100

    branchement useEffect qui recup useRef

--- hrnet/src/Components/CustomPagination/PaginationHeaderNav.jsx
+++ hrnet/src/Components/CustomPagination/PaginationHeaderNav.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setPagination } from '../../Store/slices/paginationSlice';
 
@@ -16,14 +16,31 @@ const PaginationHeaderNav = () => {
     let target = parseInt(e.target.value);
     setNumberOfRows(target);
   }
+  function change2(e) {
+    e.preventDefault();
+    // console.log(e.target.value);
+  }
+  //////////////////////////////////////
+
+  const ref = useRef('notnull');
+
+  useEffect(() => {
+    if (ref.current.value !== undefined) {
+      console.log(ref.current.value);
+    }
+  });
+
+  //////////////////////////
   return (
     <nav className='tableHeader-nav flex items-center'>
       <div className='text-neutral bold'>View</div>
       <select
         name='rows-select'
+        ref={ref}
         id='rows-select'
         onChange={change}
         defaultValue={pagination.numberOfRows}
+        //defaultValue={'all'}
         className='text-neutral rounded border border-neutral'
       >
         <option value='1'>1</option>
@@ -31,6 +48,7 @@ const PaginationHeaderNav = () => {
         <option value='10'>10</option>
         <option value='25'>25</option>
         <option value='50'>50</option>
+        <option value='all'>All</option>
       </select>
       <div className='text-neutral bold'>
         {pagination.numberOfRows === 1 ? 'entry' : 'entries'}
